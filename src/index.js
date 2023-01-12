c9a5082246e9c43af80c1b5f1a43a59454041112
Galen Sho Sato <galen.sato@gmail.com>
2023-01-12 16:21:00 -0500

    Finished the main three deliverables, and ended with the dropdown menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  
+  
+  
+  const taskForm = document.querySelector("#create-task-form")
+  taskForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    const taskValue = document.querySelector('#new-task-description')
+    const taskList = document.querySelector('#tasks')
+    const newTask = document.createElement('li')
+    newTask.textContent = taskValue.value
+
+    const priority = document.querySelector('#priority')
+    
+     if (priority.value === "High") {
+      newTask.classList.add('red')
+    } else if (priority.value === 'Medium') {
+      newTask.classList.add('yellow')
+    } else {
+      newTask.classList.add('green')
+    }
+  
+    
+    const delBtn = document.createElement('button')
+    delBtn.textContent = 'Delete'
+    delBtn.addEventListener('click', (event) => {
+      newTask.remove()
+    })
+
+
+
+    newTask.append(delBtn)
+    taskList.append(newTask)
+  } )
+
+  
 });
